1974834facf3fda34c1f2c111e95206bf593ada7
katomasahiro10 <katomasahiro10@gmail.com>
2021-09-17 16:40:31 +0900

    commit tasks.py

---
+++ tasks.py
@@ -0,0 +1,36 @@
+import math
+
+def eval_fitness(net, step:int, cycle:int) -> float:
+
+    error = 0.0
+
+    for s in range (step):
+
+        cyclic_input = s * 2 * math.pi / cycle
+        target_output = (math.sin(cyclic_input) + 1.0 ) / 2
+
+        # Get output phase
+        net_input = [ (1.0, 0.0, 0.0, 0.0)]
+        output = net.activate(net_input)
+        difference = target_output - output
+
+        # Caliculate error
+        error += math.log ( abs (target_output - output) +1 ) 
+
+        # Feedback phase
+        net_input = [ (0.0, 1.0, output, difference) ]
+        net.activate(net_input)
+
+    error /= step
+    assert(error <= 1.0)
+
+    return 1.0 - error
+
+class net_dummy:
+    def activate(self, input):
+        return 0.5
+
+if __name__=='__main__':
+    n = net_dummy()
+    fitness = eval_fitness(n, 100, 10)
+    print(fitness)
