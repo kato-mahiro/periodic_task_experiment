2dae8972a3bb99bd3c25e38ba55ee833f26f1060
katomasahiro10 <katomasahiro10@gmail.com>
2021-09-17 23:07:49 +0900

    implement 2 tasks
    sinwave and binary

--- tasks.py
+++ tasks.py
@@ -3,7 +3,7 @@ import math
 import matplotlib.pyplot as plt
 from numpy import save
 
-def eval_fitness(net, step:int, cycle:int, draw_graph=False, show_graph = False, savepath = None) -> float:
+def sinwave_task(net, step:int, cycle:int, draw_graph=False, show_graph = False, savepath = None) -> float:
 
     error = 0.0
     expected = []
@@ -33,32 +33,78 @@ def eval_fitness(net, step:int, cycle:int, draw_graph=False, show_graph = False,
     error /= step
 
     if(draw_graph):
-        #draw graph
-        x = range(1, step+1)
-        y1 = expected
-        y2 = got_output
+        draw(step = step, expected = expected, got_output = got_output, show_graph = show_graph, savepath = savepath)
 
-        fig = plt.figure()
-
-        plt.plot(x, y1, linestyle = "-", color = "blue", label = "expected output")
-        plt.plot(x, y2, linestyle = "dashed", color = "red", label = "model's output")
-        plt.grid(linestyle='dotted')
-        plt.xlabel("step")
-        plt.title("Model's behaviour")
-        plt.legend(loc = "upper left")
-        if(show_graph):
-            plt.show()
-
-        #save graph
-        if(savepath):
-            fig.savefig(savepath)
-            print('Graph is saved.')
-
-        plt.close()
 
     assert(error <= 1.0)
 
     return 1.0 - error
+
+def binary_task(net, step:int, cycle:int, draw_graph=False, show_graph = False, savepath = None) -> float:
+
+    error = 0.0
+    expected = []
+    got_output = []
+
+    step_cnt = 0
+    target_output = 0.0
+    
+    for s in range (step):
+
+        step_cnt += 1
+        if(step_cnt % cycle == 0):
+            if(target_output == 0.0):
+                target_output = 1.0
+            elif(target_output == 1.0):
+                target_output = 0.0
+
+        expected.append(target_output)
+
+        # Get output phase
+        net_input = [1.0, 0.0, 0.0, 0.0]
+        output = net.activate(net_input)[0]
+        got_output.append(output)
+        difference = target_output - output
+
+        # Caliculate error
+        error += math.log ( abs (target_output - output) +1 ) 
+
+        # Feedback phase
+        net_input = [0.0, 1.0, output, difference]
+        net.activate(net_input)
+
+    error /= step
+
+    if(draw_graph):
+        draw(step = step, expected = expected, got_output = got_output, show_graph = show_graph, savepath = savepath)
+
+
+    assert(error <= 1.0)
+
+    return 1.0 - error
+
+def draw(step, expected, got_output, show_graph, savepath):
+    x = range(1, step+1)
+    y1 = expected
+    y2 = got_output
+
+    fig = plt.figure()
+
+    plt.plot(x, y1, linestyle = "-", color = "blue", label = "expected output")
+    plt.plot(x, y2, linestyle = "dashed", color = "red", label = "model's output")
+    plt.grid(linestyle='dotted')
+    plt.xlabel("step")
+    plt.title("Model's behaviour")
+    plt.legend(loc = "upper left")
+    if(show_graph):
+        plt.show()
+
+    #save graph
+    if(savepath):
+        fig.savefig(savepath)
+        print('Graph is saved.')
+
+    plt.close()
 
 class net_dummy:
     def activate(self, input):
@@ -66,5 +112,9 @@ class net_dummy:
 
 if __name__=='__main__':
     n = net_dummy()
-    fitness = eval_fitness(n, 100, 20, show_graph=True,savepath="./hoge")
+
+    fitness = binary_task(n, 100, 20, draw_graph=True, show_graph=True,savepath="./hoge")
+    print(fitness)
+
+    fitness = sinwave_task(n, 100, 20, draw_graph=True, show_graph=True,savepath="./hoge")
     print(fitness)
