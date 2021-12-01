4f64263a0f34746e117b3548c95af08efe3ebd14
katomasahiro10 <katomasahiro10@gmail.com>
2021-12-01 22:10:28 +0900

    update: remove sinwave task
    どうせ使わんし

--- tasks.py
+++ tasks.py
@@ -3,43 +3,6 @@ import math
 import matplotlib.pyplot as plt
 from numpy import save
 
-def sinwave_task(net, step:int, cycle:int, draw_graph=False, show_graph = False, savepath = None) -> float:
-
-    error = 0.0
-    expected = []
-    got_output = []
-
-
-    for s in range (step):
-
-        cyclic_input = s * 2 * math.pi / cycle
-        target_output = (math.sin(cyclic_input) + 1.0 ) / 2
-
-        expected.append(target_output)
-
-        # Get output phase
-        net_input = [1.0, 0.0, 0.0, 0.0]
-        output = net.activate(net_input)[0]
-        got_output.append(output)
-        difference = target_output - output
-
-        # Caliculate error
-        error += math.log ( abs (target_output - output) +1 ) 
-
-        # Feedback phase
-        net_input = [0.0, 1.0, output, difference]
-        net.activate(net_input)
-
-    error /= step
-
-    if(draw_graph):
-        draw(step = step, expected = expected, got_output = got_output, show_graph = show_graph, savepath = savepath)
-
-
-    assert(error <= 1.0)
-
-    return 1.0 - error
-
 def binary_task(net, step:int, cycle:int, is_increase=False, draw_graph=False, show_graph = False, savepath = None, is_bh_only = True) -> float:
 
     error = 0.0
