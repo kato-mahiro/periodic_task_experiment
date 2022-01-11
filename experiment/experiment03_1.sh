88920655a8de9a8a6c0cdf3435bdad881462e5e5
katomasahiro10 <katomasahiro10@gmail.com>
2022-01-11 13:49:25 +0900

    delete scripts

--- experiment/experiment03_1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-dir='../results02'
-
-bash run.sh --task task.three_rules_random --network ModIndExHebbFFN --config_file ./configs/in3-out3/modindexhebb_genome.ini --savedir $dir &
-bash run.sh --task task.three_rules_cyclic --network ModIndExHebbFFN --config_file ./configs/in3-out3/modindexhebb_genome.ini --savedir $dir &
-bash run.sh --task task.three_rules_static --network ModIndExHebbFFN --config_file ./configs/in3-out3/modindexhebb_genome.ini --savedir $dir &
-bash run.sh --task task.three_rules_vary_cyclic --network ModIndExHebbFFN --config_file ./configs/in3-out3/modindexhebb_genome.ini --savedir $dir 
