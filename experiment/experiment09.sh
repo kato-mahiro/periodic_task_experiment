d815d89ccb6f981654009852045fca53a32c453d
katomasahiro10 <katomasahiro10@gmail.com>
2022-01-13 22:18:06 +0900

    setting for experiment09

---
+++ experiment/experiment09.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+dir='../results09(m_d=0)'
+
+bash run.sh --task new_task.changing_random --network ModExHebbFFN --config_file ./configs/in3-out1/modexhebb_genome.ini --savedir $dir &
+bash run.sh --task new_task.changing_static --network ModExHebbFFN --config_file ./configs/in3-out1/modexhebb_genome.ini --savedir $dir &
+bash run.sh --task new_task.changing_random_generation --network ModExHebbFFN --config_file ./configs/in3-out1/modexhebb_genome.ini --savedir $dir &
+bash run.sh --task new_task.changing_random --network ModExHebbRNN --config_file ./configs/in3-out1/modexhebb_genome_rnn.ini --savedir $dir &
+bash run.sh --task new_task.changing_static --network ModExHebbRNN --config_file ./configs/in3-out1/modexhebb_genome_rnn.ini --savedir $dir &
+bash run.sh --task new_task.changing_random_generation --network ModExHebbRNN --config_file ./configs/in3-out1/modexhebb_genome_rnn.ini --savedir $dir &
+
+bash run.sh --task new_task.timing_random --network ModExHebbFFN --config_file ./configs/in3-out1/modexhebb_genome.ini --savedir $dir &
+bash run.sh --task new_task.timing_static --network ModExHebbFFN --config_file ./configs/in3-out1/modexhebb_genome.ini --savedir $dir &
+bash run.sh --task new_task.timing_random_generation --network ModExHebbFFN --config_file ./configs/in3-out1/modexhebb_genome.ini --savedir $dir &
+bash run.sh --task new_task.timing_random --network ModExHebbRNN --config_file ./configs/in3-out1/modexhebb_genome_rnn.ini --savedir $dir &
+bash run.sh --task new_task.timing_static --network ModExHebbRNN --config_file ./configs/in3-out1/modexhebb_genome_rnn.ini --savedir $dir &
+bash run.sh --task new_task.timing_random_generation --network ModExHebbRNN --config_file ./configs/in3-out1/modexhebb_genome_rnn.ini --savedir $dir
