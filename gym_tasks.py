dde1a6453ee1f6c4adee3f7a90dc2578f34a1ece
katomasahiro10 <katomasahiro10@gmail.com>
2022-04-19 22:20:57 +0900

    cycle_cnt_maxを使うようにタスクを更新

--- gym_tasks.py
+++ gym_tasks.py
@@ -1,13 +1,18 @@
+from modneat import visualize
 import numpy
 import myenvs.myenvs as myenvs
 import gym
+import os
 
 class static_cyclic_task:
     def __init__(self, network_type):
-        self.E = gym.make('static_cyclic_env-v0', cycle = 10, action_num = 2)
         self.network_type = network_type
+        self.cycle = 10
+        self.cycle_cnt_max = 10
+        self.action_num = 2
     
     def eval_fitness(self, net):
+        self.E = gym.make('static_cyclic_env-v0', cycle = self.cycle, cytle_cnt_max = self.cycle_cnt_max, action_num = self.action_num)
         normal_fitness = 0.0
         bonus_fitness = 0.0
         history = [] #各サイクル毎の正解数、ボーナスタイミングで正解できたが分かるような履歴
@@ -17,7 +22,7 @@ class static_cyclic_task:
             #get output phase
             input_vector = [1.0, observation, 0.0, 0.0] #bias_input, observation, normal_feedbak, bonus_feedback
             action = numpy.argmax(net.activate(input_vector))
-            observation, reward, done, info = self.env.step(action)
+            observation, reward, done, info = self.E.step(action)
             if(reward == 1.0):
                 normal_fitness += 1.0
                 feedback1 = 1.0
@@ -35,11 +40,23 @@ class static_cyclic_task:
 
             if(done == True):
                 break
-
+        
         fitness = 0.0
-        fitness += normal_fitness / (200 * 2)
-        fitness += bonus_fitness / self.E.info['bonus_max'] * 2
+        fitness += normal_fitness / (self.action_num * self.cycle_num * self.cycle_cnt_max)
+        fitness += bonus_fitness / (self.E.info['bonus_max'] * 2)
         return fitness, history
+
+    def eval_genomes(self, genomes, config):
+        for genome_id, genome in genomes:
+            net = self.network_type.create(genome, config)
+            genome.fitness, genome.history = self.eval_fitness(net)
+    
+    def show_results(self, best_genome, config, stats, out_dir):
+        # Visualize the experiment results
+        node_names = {-1:'Bias input', -2: 'observation', -3: 'normal_feedback', -4:'bonus_feedback', 0:'output1', 1:'output2'}
+        visualize.draw_net(config, best_genome, False, node_names=node_names, directory=out_dir)
+        visualize.plot_stats(stats, ylog=False, view=False, filename=os.path.join(out_dir, 'avg_fitness.png'))
+        visualize.plot_species(stats, view=False, filename=os.path.join(out_dir, 'speciation.png'))
 
 class dynamic_cyclic_task:
     def __init__(self, network_type):
