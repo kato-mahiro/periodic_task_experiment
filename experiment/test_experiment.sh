0817d7b0d770aa7d99c56f4f6379f9103b2c3f68
katomasahiro10 <katomasahiro10@gmail.com>
2022-04-29 16:12:02 +0900

    edit test script

--- experiment/test_experiment.sh
+++ experiment/test_experiment.sh
@@ -2,4 +2,4 @@
 
 dir='../results_test'
 
-bash run.sh --task gym_tasks.static_cyclic_task --generation 100 --network ModExHebbRNN --config_file ./configs/in4-out1/modexhebb_genome_rnn.ini --savedir $dir --job_no 10
+bash run.sh --task gym_tasks.multi_cyclic_task --generation 10 --network ModExHebbRNN --config_file ./configs/in4-out1/modexhebb_genome_rnn.ini --savedir $dir --job_no 10
