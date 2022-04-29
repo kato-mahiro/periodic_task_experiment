020f6873a0a6b1228d8ff7d6f2f9b4f1e7168caa
katomasahiro10 <katomasahiro10@gmail.com>
2022-04-29 16:18:01 +0900

    update script

--- experiment/exp_cyclic_history.sh
+++ experiment/exp_cyclic_history.sh
@@ -2,7 +2,7 @@
 
 dir='../results_dir/2actions_noise_0_md_0'
 
-bash run.sh --task gym_tasks.single_cyclic_task --generation 2000 --network ModExHebbRNN --config_file ./configs/in4-out2/modexhebb_genome_rnn.ini --savedir $dir --job_no 10 \
-bash run.sh --task gym_tasks.multi_cyclic_task --generation 2000 --network ModExHebbRNN --config_file ./configs/in4-out2/modexhebb_genome_rnn.ini --savedir $dir --job_no 10 \
-bash run.sh --task gym_tasks.single_cyclic_task --generation 2000 --network ModExHebbFFN --config_file ./configs/in4-out2/modexhebb_genome_ffn.ini --savedir $dir --job_no 10 \
+bash run.sh --task gym_tasks.single_cyclic_task --generation 2000 --network ModExHebbRNN --config_file ./configs/in4-out2/modexhebb_genome_rnn.ini --savedir $dir --job_no 10 &\
+bash run.sh --task gym_tasks.multi_cyclic_task --generation 2000 --network ModExHebbRNN --config_file ./configs/in4-out2/modexhebb_genome_rnn.ini --savedir $dir --job_no 10 &\
+bash run.sh --task gym_tasks.single_cyclic_task --generation 2000 --network ModExHebbFFN --config_file ./configs/in4-out2/modexhebb_genome_ffn.ini --savedir $dir --job_no 10 &\
 bash run.sh --task gym_tasks.multi_cyclic_task --generation 2000 --network ModExHebbFFN --config_file ./configs/in4-out2/modexhebb_genome_ffn.ini --savedir $dir --job_no 10
